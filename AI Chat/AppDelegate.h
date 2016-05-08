d72d186757a713b9b218ac378a3b1795110302a9
Julian Gutierrez <julian.3.1@gmail.com>
2016-05-08 15:32:12 -0300

    Project setup

---
+++ AI Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AI Chat
+//
+//  Created by Julian Gutierrez on 5/8/16.
+//  Copyright © 2016 Company Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AI Chat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AI Chat
+//
+//  Created by Julian Gutierrez on 5/8/16.
+//  Copyright © 2016 Company Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
